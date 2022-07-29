3b77018508e115514759f7ecc9909ce037272f8a
Cauan Victor <cauanvictor0325@gmail.com>
2022-07-28 23:15:37 -0300

    👨‍🦰 Responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,16 +1,22 @@
 import { Flex, Box, Text, Avatar } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({showProfileData = true}: ProfileProps) {
     return(
         <Flex align="center">
-                    <Box mr="4" textAlign="right">
-                        <Text>Cauan Victor</Text>
-                        <Text color="gray.400" fontSize="small">
-                            francisco.cauan.alu.lmb@gmail.com
-                        </Text>
-                    </Box>
+            { showProfileData && (
+                <Box mr="4" textAlign="right">
+                    <Text>Cauan Victor</Text>
+                    <Text color="gray.400" fontSize="small">
+                        francisco.cauan.alu.lmb@gmail.com
+                    </Text>
+                </Box>
+            )}
 
-                    <Avatar size="md" name="Cauan Victor" src="https://github.com/CauanDZN.png" />
-                </Flex>
+            <Avatar size="md" name="Cauan Victor" src="https://github.com/CauanDZN.png" />
+        </Flex>
     );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,15 @@
-import { Flex } from "@chakra-ui/react"
+import { Flex, useBreakpointValue } from "@chakra-ui/react"
 import { Logo } from "./Logo"
 import { NotificationNav } from "./NotificationNav"
 import { Profile } from "./Profile"
 import { SearchBox } from "./SearchBox"
 
 export function Header(){
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true,
+    })
+    
     return(
         <Flex 
             as="header" 
@@ -18,7 +23,7 @@ export function Header(){
         >
             <Logo />
 
-            <SearchBox />
+            { isWideVersion && <SearchBox /> }
 
             <Flex 
                 align="center"
@@ -27,7 +32,7 @@ export function Header(){
 
                 <NotificationNav />
 
-                <Profile />
+                <Profile showProfileData={isWideVersion} />
                 
             </Flex>
         </Flex>
